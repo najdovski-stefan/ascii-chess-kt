53c45cf1f4d1864e020d3f52c444ecb632574202
Stefan <stefan.najdovski.git@gmail.com>
2025-12-24 12:49:03 +0100

    board with rooks

--- ChessGame.kt
+++ ChessGame.kt
@@ -4,8 +4,18 @@ fun main(){
     val board = Array(8){
         Array(8){'.'}
     }
+    //TOP
+    board[0][0] = 'T'
+    board[0][7] = 'T'
+    board[7][0] = 'T'
+    board[7][7] = 'T'
 
-    board[0][0] = 'K'
+    for (row in board) {
+         for (square in row) {
+             print("$square ")
+         }
 
-    println(board[0].contentToString())
+         println()
+     }
+
 }
