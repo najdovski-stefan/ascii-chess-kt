4337657208eadcf691556c5ad6168c95dbc119a0
Stefan <stefan.najdovski.git@gmail.com>
2025-12-28 13:51:57 +0100

    ansi-board test

--- ChessGame.kt
+++ ChessGame.kt
@@ -1,60 +1,84 @@
 enum class Color {
     WHITE,
     BLACK
-};
-
-data class Piece(val name:String,val color:Color)
-
-fun main(){
-
-val board = Array(8){
-    arrayOfNulls<Piece>(8)
-};
-
-while(true){
-    for(i in 0..7){
-        for(j in 0..7){
-            val piece = board[i][j]
-
-            if(piece == null){
-                print(". ")
-            }else{
-
-               print(piece.name+" ")
-            }
-        }
-        println()
-    }
-    print("Enter coordinate")
-    println(" Input : 0,0 1,0")
-    //take from 0,0 to 1,0
-    val from_where = readln()
-    val spaceSplit = from_where.split(" ")
-    val firstPair = spaceSplit[0]
-    val secPair = spaceSplit[1]
-    println(firstPair)
-    println(secPair)
-    val firstSplit = firstPair.split(",")
-    val secSplit = secPair.split(",")
-
-    val rowFirstPair = firstSplit[0].toInt()
-    val colFirstPair = firstSplit[1].toInt()
-    val rowSecondPair = secSplit[0].toInt()
-    val colSecondPair = secSplit[1].toInt()
-    println("First row:$rowFirstPair")
-    println("First col:$colFirstPair")
-    println("Sec row:$rowSecondPair")
-    println("Sec col:$colSecondPair")
-
-    val pieceTest =  board[rowFirstPair][colFirstPair]
-
-    if(pieceTest == null){
-         board[rowFirstPair][colFirstPair] = Piece("T",Color.WHITE)
-    }else{
-
-    }
-
-
-
 }
+
+data class Piece(val name: String, val color: Color)
+
+object Ansi {
+    const val RESET = "\u001b[0m"
+
+    //https://www.ditig.com/256-colors-cheat-sheet
+    const val WHITE_TXT = "\u001b[37;1m" // W
+    const val BLACK_TXT = "\u001b[30m"   // B
+    const val RED_TXT = "\u001b[31m"     // R
+
+
+    const val BG_LIGHT = "\u001b[47m"    // W BG
+    const val BG_DARK = "\u001b[100m"    // D BG
+}
+
+fun main() {
+
+    val board = Array(8) {
+        arrayOfNulls<Piece>(8)
+    }
+
+    board[0][0] = Piece("T", Color.BLACK)
+    board[7][7] = Piece("T", Color.WHITE)
+
+    while (true) {
+        println("   0  1  2  3  4  5  6  7")
+
+        for (i in 0..7) {
+            print("$i ")
+
+            for (j in 0..7) {
+                val piece = board[i][j]
+
+                //crno/belo
+                val bg = if ((i + j) % 2 == 0) Ansi.BG_LIGHT else Ansi.BG_DARK
+
+                val textToPrint: String
+
+                if (piece == null) {
+                    textToPrint = "${bg}   ${Ansi.RESET}"
+                } else {
+                    val fg = if (piece.color == Color.WHITE) Ansi.WHITE_TXT else Ansi.BLACK_TXT
+                    textToPrint = "${bg}${fg} ${piece.name} ${Ansi.RESET}"
+                }
+
+                print(textToPrint)
+            }
+            println()
+        }
+
+        println("Vnesi Koordinati (0,0 1,0)")
+
+        try {
+            val from_where = readln()
+            if (from_where == "exit") break
+
+            val spaceSplit = from_where.split(" ")
+            val p1 = spaceSplit[0].split(",")
+            val p2 = spaceSplit[1].split(",")
+
+            val r1 = p1[0].toInt();
+            val c1 = p1[1].toInt()
+            val r2 = p2[0].toInt();
+            val c2 = p2[1].toInt()
+
+
+            if (board[r1][c1] != null) {
+                board[r2][c2] = board[r1][c1]
+                board[r1][c1] = null
+            } else {
+                // If empty, just spawn a test piece as per your logic
+                board[r1][c1] = Piece("T", Color.WHITE)
+            }
+
+        } catch (e: Exception) {
+            println("Invalid Input, try format: 0,0 1,1")
+        }
+    }
 }
