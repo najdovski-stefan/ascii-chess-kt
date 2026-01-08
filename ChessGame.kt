4972683d40f76ca4a6ad23bfcfa120d7b587ffff
Stefan <stefan.najdovski.git@gmail.com>
2026-01-08 16:23:52 +0100

    feat: board:

--- ChessGame.kt
+++ ChessGame.kt
@@ -1,65 +1,63 @@
-enum class Color{WHITE,BLACK}//https://kotlinlang.org/docs/enum-classes.html
-data class Piece(val name:String,val color:Color);//https://kotlinlang.org/docs/data-classes.html
 
-fun main(){
+enum class Color{
+	WHITE,
+	BLACK
+}
+
+enum class PieceType{
+	PAWN,
+	KNIGHT,
+	BISHOP,
+	KING,
+	QUEEN,
+	ROOK,
+}
 
 
-    val board = Array(8) {
-        arrayOfNulls<Piece>(8)//https://kotlinlang.org/api/core/kotlin-stdlib/kotlin/array-of-nulls.html
+//toString, equals, hashcode java manual, kotlin auto
+data class Piece(val type:PieceType, val color: Color)
+
+
+//piece might be nullable
+data class Board(
+
+var grid:Array<Array<Piece?>> = Array(8){arrayOfNulls(8)}
+
+
+){
+
+	init {
+
+	val rowLayout = listOf(PieceType.ROOK,PieceType.KNIGHT,PieceType.BISHOP,PieceType.QUEEN,PieceType.KING,PieceType.BISHOP,PieceType.KNIGHT,PieceType.ROOK)
+
+    for(i in 0..7){
+        grid[0][i] = Piece(rowLayout[i],Color.BLACK)
+        grid[1][i] = Piece(PieceType.PAWN,Color.BLACK)
+        grid[6][i] = Piece(PieceType.PAWN, Color.WHITE)
+        grid[7][i] = Piece(rowLayout[i],Color.WHITE)
+
     }
 
-    val belTop = Piece("T",Color.WHITE)
-    val crnTop = Piece("T",Color.BLACK)
+	}
 
-    board[0][0] = belTop
-    board[7][0] = crnTop
-
-    for (row in board) {
-        for (square in row) {
-            if (square == null) {
-                print(". ")
-            } else {
-                //https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.text/uppercase.html
-                // https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.text/lowercase.html
-                val letter = if (square.color == Color.WHITE) square.name.uppercase() else square.name.lowercase()
-                print("$letter ")
-            }
-        }
-        println()
-    }
-
-    println("Vnesi Koordinati 1,1 :")
+	fun printBoard(){
+	    //8*8 = 64 fields
+	    for(row in 0..7){
+			for(col in 0..7){
+			if(	grid[row][col] == null){
+			    print(". ")
+			}else{
+			    print("${grid[row][col]}")
+			}
+			}
+			println("")
+		}
 
 
-    val input = readln()// https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.io/readln.html
+	}
+
+	private fun getSymbol(piece: Piece): String {
 
 
-    val parts:List<String> = input.split(",") // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.text/split.html
-
-    println("Ti napisa $input")
-    println("Redica: " + parts[0])
-    println("Kolona: " + parts[1])
-
-    // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.text/to-int.html
-    val rowIdx = parts[0].toInt()
-    val colIdx = parts[1].toInt()
-
-    // Piunce
-    board[rowIdx][colIdx] = Piece("P", Color.WHITE)
-
-    for (row in board) {
-        for (square in row) {
-            if (square == null) {
-                print(". ")
-            } else {
-                //https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.text/uppercase.html
-                // https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.text/lowercase.html
-                val letter = if (square.color == Color.WHITE) square.name.uppercase() else square.name.lowercase()
-                print("$letter ")
-            }
-        }
-        println()
-    }
-
-
+	}
 }
